65c9819e31a5f00bafa16a70a74ed59fe8fa1d27
moises.neto <moises.neto@animaeducacao.com.br>
2023-08-21 20:29:57 -0300

    \z

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   devServer: {
-    proxy: 'https://servico-orcamento-generico.azurewebsites.net/',
-    },
+    proxy: {
+      '^/api': {
+        target: 'https://servico-orcamento-generico.azurewebsites.net', // URL da sua API
+        changeOrigin: true,
+        secure: true, // Ajuste para true se o seu servidor de destino também usa HTTPS
+        pathRewrite: {
+          '^/api': '/api' // Mantenha o prefixo "/api" na URL da requisição
+        }
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
